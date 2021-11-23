024e15e8c6bc1b5ae56ed288eb708af81aaed97b
Shribee NUC <sbalanchickoo@gmail.com>
2021-11-23 08:42:47 +0000

    Update hl-scraper.py

--- src/hl-scraper.py
+++ src/hl-scraper.py
@@ -5,20 +5,10 @@ across funds
 
 Arguments:
 root_path -- where account summary and investment files are stored
-Pre-requisite steps:
-1. Log into Hargreaves Lansdown account
-2. Download Account summary as csv files into a folder
-    a. Folder path: /root_path/Account-Summary/
-    b. File name format: <yyyymmdd-<account_type>-accountsummary.csv
-    c. account_type: isa/sipp
-3. Delete all extra header rows, footer rows, totals row
-4. For each investment (fund/stock), open fact sheet in new tab
-5. Download page as webpage complete format
-    a. Folder path: /root_path/Investments/<yyyy-mm-dd>/
-    b. File name format: <name of investment>.html
 
 Script steps:
-1. Execute hl-scraper with root_path as argument
+1. Perform pre-requisite steps as per readme doc
+2. Execute python script/docker container with root_path as argument
 
 """
 
